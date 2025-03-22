1eff46b16cc881213b8cc2e2f1fc36e0cb7eb76a
Ishan <ishanagrawal234.com>
2025-03-22 20:36:25 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ishan',
+    author_email= 'ishanagrawal234@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
